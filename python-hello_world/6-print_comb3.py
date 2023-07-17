93e4b39ac7a46437bdd0e495eb8672eec99f8b83
Ogunleye Daniel <ogundan88@gmail.com>
2023-07-17 03:45:27 -0700

    Update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,7 +3,7 @@ i = 0
 j = 1
 while i < 9:
     while j < 10:
-        print("{:02d}, ".format(i * 10 + j), end="" if i * 10 + j == 89 else " ")
+        print("{:02d},".format(i * 10 + j), end="" if i * 10 + j == 89 else " ")
         j += 1
     i += 1
     j = i + 1
